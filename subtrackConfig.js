74cb014199dcb617e83d0991ae867e54c5568942
NatiPerez459 <70607250+NatiPerez459@users.noreply.github.com>
2021-01-09 18:57:50 -0400

    Firebase files
    Error with auth is not defined

---
+++ subtrackConfig.js
@@ -0,0 +1,15 @@
+
+ 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBoePGNuYbAVDtnuRDLjj0bgxBRSaBMFdc",
+    authDomain: "subtrack-44207.firebaseapp.com",
+    projectId: "subtrack-44207",
+    storageBucket: "subtrack-44207.appspot.com",
+    messagingSenderId: "597619555514",
+    appId: "1:597619555514:web:a42e39858752fede6e9683"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ 
